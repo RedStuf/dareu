14d9f694b57d9f0c513d2bffe78150ff41d49201
DhirajSanchit <Dhirajsanchit@gmail.com>
2014-06-17 01:11:08 +0200

    xml layout van de challenge description
    Moet nog meer bij, wordt aan gewerkt

--- Eclipse/DareU/gen/com/phonezilla/dareu/R.java
+++ Eclipse/DareU/gen/com/phonezilla/dareu/R.java
@@ -21,6 +21,7 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int font_scoreView=0x7f050002;
     }
     public static final class drawable {
         public static final int download=0x7f020000;
@@ -30,32 +31,38 @@ public final class R {
         public static final int stamp2=0x7f020004;
     }
     public static final class id {
-        public static final int acceptedchallenges=0x7f090003;
-        public static final int button1=0x7f090002;
-        public static final int camera_surface_view=0x7f090004;
-        public static final int checkBox=0x7f09000a;
-        public static final int comp=0x7f090007;
-        public static final int grouplayout=0x7f090009;
+        public static final int acceptedchallenges=0x7f090008;
+        public static final int button1=0x7f090007;
+        public static final int camera_surface_view=0x7f090009;
+        public static final int challengeNameView=0x7f090002;
+        public static final int checkBox=0x7f09000f;
+        public static final int comp=0x7f09000c;
+        public static final int grouplayout=0x7f09000e;
         public static final int imageView=0x7f090000;
-        public static final int newchallenge=0x7f09000b;
-        public static final int newgroup=0x7f09000c;
+        public static final int newchallenge=0x7f090010;
+        public static final int newgroup=0x7f090011;
         public static final int pager=0x7f090001;
-        public static final int photo_button=0x7f090005;
-        public static final int rank=0x7f090008;
-        public static final int tdo=0x7f090006;
+        public static final int photo_button=0x7f09000a;
+        public static final int previewEvedince_button=0x7f090006;
+        public static final int rank=0x7f09000d;
+        public static final int scoreView=0x7f090003;
+        public static final int tdo=0x7f09000b;
+        public static final int textView2=0x7f090004;
+        public static final int upload_button=0x7f090005;
     }
     public static final class layout {
         public static final int activity_begin=0x7f030000;
         public static final int activity_group=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int fragment_accepted=0x7f030003;
-        public static final int fragment_camera=0x7f030004;
-        public static final int fragment_challenges=0x7f030005;
-        public static final int fragment_completed=0x7f030006;
-        public static final int fragment_friends=0x7f030007;
-        public static final int fragment_groups=0x7f030008;
-        public static final int fragment_pending=0x7f030009;
-        public static final int fragment_settings=0x7f03000a;
+        public static final int challenge_description=0x7f030003;
+        public static final int fragment_accepted=0x7f030004;
+        public static final int fragment_camera=0x7f030005;
+        public static final int fragment_challenges=0x7f030006;
+        public static final int fragment_completed=0x7f030007;
+        public static final int fragment_friends=0x7f030008;
+        public static final int fragment_groups=0x7f030009;
+        public static final int fragment_pending=0x7f03000a;
+        public static final int fragment_settings=0x7f03000b;
     }
     public static final class menu {
         public static final int challengemenu=0x7f080000;
@@ -64,6 +71,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int button_viewProof=0x7f060004;
         public static final int hello_blank_fragment=0x7f060003;
         public static final int hello_world=0x7f060001;
     }
