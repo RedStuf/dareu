8cce5be6ec9189a8dbf5eb28c49c845cb3033b88
Wouter Naloop <Baas@hotmail.com>
2014-06-19 12:29:49 +0200

    Generated files change

--- Eclipse/DareU/gen/com/phonezilla/dareu/R.java
+++ Eclipse/DareU/gen/com/phonezilla/dareu/R.java
@@ -25,6 +25,7 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int font_scoreView=0x7f050002;
     }
     public static final class drawable {
         public static final int button_login=0x7f020000;
@@ -39,43 +40,49 @@ public final class R {
         public static final int table_round_shape=0x7f020009;
     }
     public static final class id {
-        public static final int acceptedchallenges=0x7f090004;
-        public static final int button1=0x7f090003;
-        public static final int camera_surface_view=0x7f090005;
-        public static final int checkBox=0x7f09000b;
-        public static final int comp=0x7f090008;
-        public static final int grouplayout=0x7f09000a;
+        public static final int acceptedchallenges=0x7f090009;
+        public static final int button1=0x7f090008;
+        public static final int camera_surface_view=0x7f09000a;
+        public static final int challengeNameView=0x7f090002;
+        public static final int checkBox=0x7f090010;
+        public static final int comp=0x7f09000d;
+        public static final int grouplayout=0x7f09000f;
         public static final int imageView=0x7f090000;
-        public static final int loginButton=0x7f090002;
-        public static final int logoutButton=0x7f090012;
-        public static final int newchallenge=0x7f090013;
-        public static final int newgroup=0x7f090014;
+        public static final int loginButton=0x7f090007;
+        public static final int logoutButton=0x7f090017;
+        public static final int newchallenge=0x7f090018;
+        public static final int newgroup=0x7f090019;
         public static final int pager=0x7f090001;
-        public static final int photo_button=0x7f090006;
-        public static final int rank=0x7f090009;
-        public static final int tdo=0x7f090007;
-        public static final int userDateOfBirth=0x7f090010;
-        public static final int userGender=0x7f09000f;
-        public static final int userLocation=0x7f09000e;
-        public static final int userName=0x7f09000d;
-        public static final int userProfilePicture=0x7f09000c;
-        public static final int userRelationship=0x7f090011;
+        public static final int photo_button=0x7f09000b;
+        public static final int previewEvedince_button=0x7f090004;
+        public static final int rank=0x7f09000e;
+        public static final int scoreView=0x7f090006;
+        public static final int tdo=0x7f09000c;
+        public static final int textView2=0x7f090005;
+        public static final int upload_button=0x7f090003;
+        public static final int userDateOfBirth=0x7f090015;
+        public static final int userGender=0x7f090014;
+        public static final int userLocation=0x7f090013;
+        public static final int userName=0x7f090012;
+        public static final int userProfilePicture=0x7f090011;
+        public static final int userRelationship=0x7f090016;
     }
     public static final class layout {
         public static final int activity_begin=0x7f030000;
         public static final int activity_group=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int facebook=0x7f030003;
-        public static final int facebookmain=0x7f030004;
-        public static final int fragment_accepted=0x7f030005;
-        public static final int fragment_camera=0x7f030006;
-        public static final int fragment_challenges=0x7f030007;
-        public static final int fragment_completed=0x7f030008;
-        public static final int fragment_friends=0x7f030009;
-        public static final int fragment_groups=0x7f03000a;
-        public static final int fragment_pending=0x7f03000b;
-        public static final int fragment_settings=0x7f03000c;
-        public static final int userdetails=0x7f03000d;
+        public static final int challenge_description=0x7f030003;
+        public static final int facebook=0x7f030004;
+        public static final int facebookmain=0x7f030005;
+        public static final int fragment_accepted=0x7f030006;
+        public static final int fragment_camera=0x7f030007;
+        public static final int fragment_challenges=0x7f030008;
+        public static final int fragment_completed=0x7f030009;
+        public static final int fragment_friends=0x7f03000a;
+        public static final int fragment_groups=0x7f03000b;
+        public static final int fragment_pending=0x7f03000c;
+        public static final int fragment_settings=0x7f03000d;
+        public static final int userdetails=0x7f03000e;
     }
     public static final class menu {
         public static final int challengemenu=0x7f080000;
@@ -85,6 +92,7 @@ public final class R {
         public static final int action_settings=0x7f060002;
         public static final int app_id=0x7f060004;
         public static final int app_name=0x7f060000;
+        public static final int button_viewProof=0x7f06000c;
         public static final int date_of_birth=0x7f06000a;
         public static final int gender=0x7f060009;
         public static final int hello_blank_fragment=0x7f060003;
